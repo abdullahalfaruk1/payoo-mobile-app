b0702739ab4387fd8cee6d2e6f3da2d007d0ce83
abdullahalfaruk1 <alfaruk091@gmail.com>
2026-05-19 20:59:17 +0600

    added login new page file

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // login button funtionality
 document.getElementById("loginButton")
 .addEventListener("click",function(e){
-    // console.log("login button click");
     e.preventDefault();
+    const mobileNumber = 01729530632
+    const pinNumber = 1234
+    const mobileNumberValue =document.getElementById("mobile-number").value
+    const mobileNumberValueCoverted = parseInt(mobileNumberValue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    if(mobileNumberValueCoverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        window.location.href = "./home.html"
+    }
+else{
+    alert("Invalid Credentials")
+}
 })
